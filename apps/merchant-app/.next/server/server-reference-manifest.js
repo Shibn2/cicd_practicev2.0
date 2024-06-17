147548dabdfd293dd21a89878fcfbc3e7c794320
SHIBINLAL <shibinlal@SHIBINLALs-MacBook-Pro.local>
2024-06-17 10:11:59 +0530

    slal-cicd-01 | init commit

---
+++ apps/merchant-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"mup8lCZ8CpmvaX2Nu4zZdH25t6Rur8AEzsrsXuTpMJU=\"}"
---
+++ apps/user-app/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/shibinlal/Documents/Shibin/Study/Code_Repos/hkiratProjects/week-17-cicd/CI_CD_Practice/apps/user-app/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
